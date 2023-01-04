429ab3d7802e1827fd6832651566f2de37fd8974
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2023-01-04 20:20:48 -0300

    Update itemid_repository.hpp

--- inc/managers/itemid_repository.hpp
+++ inc/managers/itemid_repository.hpp
@@ -19,35 +19,35 @@ class ItemIdRepository {
   // Item Id
 
   // Blocks
-  Item dirt 1;
-  Item sand 3;
-  Item stone 2;
-  Item bricks 4;
-  Item glass 5;
+  Item dirt;
+  Item sand;
+  Item stone;
+  Item bricks;
+  Item glass;
 
   // Ores and Minerals blocks
-  Item coal_ore_block 6;
-  Item diamond_ore_block 9;
-  Item iron_ore_block 7;
-  Item gold_ore_block 8;
-  Item redstone_ore_block 10;
-  Item emerald_ore_block 11;
+  Item coal_ore_block;
+  Item diamond_ore_block;
+  Item iron_ore_block;
+  Item gold_ore_block;
+  Item redstone_ore_block;
+  Item emerald_ore_block;
 
   // Wood Planks
-  Item oak_planks 12;
-  Item spruce_planks 13;
-  Item birch_planks 14;
-  Item acacia_planks 15;
+  Item oak_planks;
+  Item spruce_planks;
+  Item birch_planks;
+  Item acacia_planks;
 
   // Stone Bricks
-  Item stone_brick 16;
-  Item cracked_stone_bricks 17;
-  Item mossy_stone_bricks 18;
-  Item chiseled_stone_bricks 19;
+  Item stone_brick;
+  Item cracked_stone_bricks;
+  Item mossy_stone_bricks;
+  Item chiseled_stone_bricks;
 
   // Stripped woods
-  Item stripped_oak_wood 20;
+  Item stripped_oak_wood;
 
   // Toolds
-  Item wooden_axe 21;
+  Item wooden_axe;
 };
